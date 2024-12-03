38ccced28076a6ce58fbf03ec1f856ac6a4d00b3
Rahul M <rahulm0409@gmail.com>
2024-12-03 10:38:33 +0530

    Added apiToken.salt configuration for Strapi

--- backend/config/admin.ts
+++ backend/config/admin.ts
@@ -1,9 +1,28 @@
+// export default ({ env }) => ({
+//   auth: {
+//     secret: env('ADMIN_JWT_SECRET'),
+//   },
+//   apiToken: {
+//     salt: env('API_TOKEN_SALT'),
+//   },
+//   transfer: {
+//     token: {
+//       salt: env('TRANSFER_TOKEN_SALT'),
+//     },
+//   },
+//   flags: {
+//     nps: env.bool('FLAG_NPS', true),
+//     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+//   },
+// });
+
+
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'H2IHvnFI85xhWQFW/S6MRQ=='), // Use your generated salt
   },
   transfer: {
     token: {
